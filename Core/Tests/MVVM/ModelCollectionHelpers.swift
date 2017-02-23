3859be502c4bfbf16e481991956aa691eb2818c0
Daniel Hammond <danielrhammond@gmail.com>
2017-02-23 10:57:50 -0800

    Add test coverage for scored and multiplex model collections (#5)
    Shook out one inconsistency with multiplex model collections preventing it from sharing data from any sections until all were loaded.

---
+++ Core/Tests/MVVM/ModelCollectionHelpers.swift
@@ -0,0 +1,48 @@
+import XCTest
+@testable import Pilot
+
+/// Fails test case if `actual` model collection state isn't equivelant to `expected`.
+///
+/// Checks that the case for both is the same (ex: .loading(*) != .loaded(*)), but if the case is the same then it
+/// checks the sections have same number of models with the same model ids.
+internal func assertModelCollectionState(
+    expected: ModelCollectionState,
+    actual: ModelCollectionState,
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    switch expected {
+    case .notLoaded:
+        if case .notLoaded = actual { return }
+        XCTFail("Expected .notLoaded state, got \(actual)", file: file, line: line)
+    case .error:
+        if case .error = actual { return }
+        XCTFail("Expected .error state, got \(actual)", file: file, line: line)
+    case .loading(let expectedSections):
+        if case .loading(let actualSections) = actual {
+            XCTAssertEqual(
+                expectedSections?.count,
+                actualSections?.count,
+                "Expected .loading with \(expectedSections?.count) sections, got \(actualSections?.count)")
+            if let expectedSections = expectedSections, let actualSections = actualSections {
+                for (e, a) in zip(expectedSections, actualSections) {
+                    XCTAssertEqual(e.map({ $0.modelId }), a.map({ $0.modelId }))
+                }
+            }
+        } else {
+            XCTFail("Expected .error state, got \(actual)", file: file, line: line)
+        }
+    case .loaded(let expectedSections):
+        if case .loaded(let actualSections) = actual {
+            XCTAssertEqual(
+                expectedSections.count,
+                actualSections.count,
+                "Expected .loaded with \(expectedSections.count) sections, got \(actualSections.count)")
+            for (e, a) in zip(expectedSections, actualSections) {
+                XCTAssertEqual(e.map({ $0.modelId }), a.map({ $0.modelId }))
+            }
+        } else {
+            XCTFail("Expected .error state, got \(actual)", file: file, line: line)
+        }
+    }
+}
---
+++ Core/Tests/MVVM/ScoredModelCollectionTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+@testable import Pilot
+
+class ScoredModelCollectionTests: XCTestCase {
+
+    func testPassthroughSourceCollection() {
+        let stub1 = StaticModel(modelId: "1", data: "")
+        let stub2 = StaticModel(modelId: "2", data: "")
+        let stub3 = StaticModel(modelId: "3", data: "")
+        let source = StaticModelCollection(collectionId: "source", initialData: [[stub1, stub2, stub3]])
+        let subject = ScoredModelCollection(source)
+        assertModelCollectionState(expected: source.state, actual: subject.state)
+    }
+
+    func testSortsByScore() {
+        let scorer: (Model) -> Double? = { model in
+            return Double(model.modelId)
+        }
+        let stub1 = StaticModel(modelId: "1", data: "")
+        let stub2 = StaticModel(modelId: "2", data: "")
+        let stub3 = StaticModel(modelId: "3", data: "")
+        let source = StaticModelCollection(collectionId: "source", initialData: [[stub1, stub2, stub3]])
+        let subject = ScoredModelCollection(source)
+        subject.scorer = scorer
+        let expected = ModelCollectionState.loaded([[stub3, stub2, stub1]])
+        assertModelCollectionState(expected: expected, actual: subject.state)
+    }
+
+    func testEnforcesLimit() {
+        let scorer: (Model) -> Double? = { model in
+            return Double(model.modelId)
+        }
+        let stub1 = StaticModel(modelId: "1", data: "")
+        let stub2 = StaticModel(modelId: "2", data: "")
+        let stub3 = StaticModel(modelId: "3", data: "")
+        let stub4 = StaticModel(modelId: "3", data: "")
+        let source = StaticModelCollection(collectionId: "source", initialData: [[stub1, stub2], [stub3, stub4]])
+        let subject = ScoredModelCollection(source)
+        subject.sectionLimit = 1
+        subject.scorer = scorer
+        let expected = ModelCollectionState.loaded([[stub2], [stub4]])
+        assertModelCollectionState(expected: expected, actual: subject.state)
+    }
+}
