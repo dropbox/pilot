b690e8d064d44d2c9a59a88ff8c65bf25086848c
Daniel Hammond <danielrhammond@gmail.com>
2017-06-06 22:16:18 -0700

    Revert "Build on Xcode 9 beta"
    This reverts commit 3b2646e4a834c1d6cf6df32abdcfd76171b7353b.

--- UI/Source/CollectionViews/CollectionViewInternals.swift
+++ UI/Source/CollectionViews/CollectionViewInternals.swift
@@ -32,6 +32,7 @@ public struct CollectionZeroItemViewModel: ViewModel {
 
     // MARK: ViewModel
 
+    @available(*, unavailable, message: "Unsupported initializer")
     public init(model: Model, context: Context) {
         fatalError("Unsupported initializer")
     }
--- UI/Source/Extensions/Collection.swift
+++ UI/Source/Extensions/Collection.swift
@@ -1,4 +1,4 @@
-internal extension Collection {
+internal extension Collection where Indices.Iterator.Element == Index {
 
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
     internal subscript (safe index: Index) -> Generator.Element? {
