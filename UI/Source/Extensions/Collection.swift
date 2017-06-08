319c9c737a363b7d2f8e366a5135c3f37a9c0e84
Will Kiefer <will@kiefer.io>
2017-06-07 20:06:19 -0700

    Fixes for Xcode 9 beta 1 and Swift 3.2 (#60)
    • Fixes https://github.com/dropbox/pilot/issues/57

--- UI/Source/Extensions/Collection.swift
+++ UI/Source/Extensions/Collection.swift
@@ -1,3 +1,12 @@
+#if swift(>=3.2)
+internal extension Collection {
+
+    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+    internal subscript (safe index: Index) -> Generator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+#else
 internal extension Collection where Indices.Iterator.Element == Index {
 
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
@@ -5,3 +14,4 @@ internal extension Collection where Indices.Iterator.Element == Index {
         return indices.contains(index) ? self[index] : nil
     }
 }
+#endif
