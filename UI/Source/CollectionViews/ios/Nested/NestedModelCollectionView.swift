c03f2ddf186d49457c56100a9f376569c2f24ef0
Matthew Valentine <mattyv@dropbox.com>
2017-08-30 13:06:56 -0700

    Log fatal errors before crashing

--- UI/Source/CollectionViews/ios/Nested/NestedModelCollectionView.swift
+++ UI/Source/CollectionViews/ios/Nested/NestedModelCollectionView.swift
@@ -24,7 +24,7 @@ public final class NestedModelCollectionView: UICollectionView, View {
 
     @available(*, unavailable, message: "init(coder:) has not been implemented")
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        Log.fatal(message: "init(coder:) has not been implemented")
     }
 
     // MARK: CollectionView
