74fa27f85d61c60e0e09becaa88ee29ef5c03ea4
Daniel Hammond <danielh@dropbox.com>
2018-09-07 13:18:05 -0700

    Re-add ObservableData + Variable for non-rx builds

---
+++ Core/Tests/Observable/ObservableTests.swift
@@ -0,0 +1,48 @@
+#if canImport(RxSwift)
+// Don't test ObservableVariable when missing.
+#else
+@testable import Pilot
+import XCTest
+
+class SemaphoreExpectation {
+    init(_ testCase: XCTestCase) {
+        self.testCase = testCase
+    }
+
+    func inc() {
+        let expectation = testCase.expectation(description: "semaphore")
+        expectations.append(expectation)
+    }
+
+    func dec() {
+        expectations.popLast()!.fulfill()
+    }
+
+    private let testCase: XCTestCase
+    private var expectations: [XCTestExpectation] = []
+}
+
+class ObservableTests: XCTestCase {
+    var observer: Observer?
+
+    func testObservableVariable() {
+        let variable = ObservableVariable<String>.make(withEquatable: "initial")
+        var events = [String]()
+
+        let expectation = SemaphoreExpectation(self)
+
+        observer = variable.observe({ event in
+            events.append(event)
+            expectation.dec()
+        })
+        expectation.inc()
+        variable.data = "hello"
+        expectation.inc()
+        variable.data = "world"
+
+        waitForExpectations(timeout: 10, handler: nil)
+
+        XCTAssertEqual(["hello", "world"], events)
+    }
+}
+#endif
